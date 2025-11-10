b620ef23121b51668418cb7f77a21aca824c0755
Maksymilian Wiekiera <maksymilian3563@gmail.com>
2025-11-10 11:40:51 +0100

    Polytope class skeleton

--- src/mplusa/minplus/geometry.py
+++ src/mplusa/minplus/geometry.py
@@ -10,8 +10,7 @@ from .minplus import add_matrices
 class Cone:
 
     def __init__(self, *vectors : Collection[float|int]) -> None:
-        for vector in vectors:
-            validate_domain(vector)
+        validate_domain(vectors)
         self.vectors = np.array(vectors)  # generators of the cone
         self.vector_count = self.vectors.shape[0]
         self.dimensions = self.vectors.shape[1]
@@ -31,3 +30,41 @@ class Cone:
             result.append(self.get_point([float(s) for s in constants]))
         result = np.array(result)
         return result
+
+
+class Polytope:
+
+    def __init__(self, *faces_collection : Collection, adjust_coordinate_dimensions : bool = True) -> None:
+        self.structure = {}
+        for rank, faces in enumerate(faces_collection):
+            if not rank:
+                if adjust_coordinate_dimensions:
+                    faces = [(0, *face) for face in faces]
+                validate_domain(faces)
+            self.structure[rank] = faces
+        self._lift_vertices()
+        self.dimension = len(self.structure) - 1
+
+    def _lift_vertices(self) -> None:
+        pass
+
+    @property
+    def vertices(self) -> list[tuple]:
+        return self.structure[0]
+
+    @property
+    def edges(self) -> Collection:
+        result = []
+        for vertices_identifiers in self.structure[1]:
+            result.append([])
+            for identifier in vertices_identifiers:
+                result[-1].append(self.vertices[identifier])
+        return result
+
+    def get_hyperplanes(self) -> list:
+        """ Returns a list of coefficients of the facet-defining hyperplanes. """
+        pass
+
+    def get_apices(self) -> list:
+        """ Returns a list of apices corresponding to the half-spaces defined by the facet-defining hyperplanes. """
+        pass
