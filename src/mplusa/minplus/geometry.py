8c8306de8020d2f6c492e08cad81433f86a47f2e
Maksymilian Wiekiera <maksymilian3563@gmail.com>
2025-12-01 11:04:28 +0100

    Point types and WIP pseudovertices

--- src/mplusa/minplus/geometry.py
+++ src/mplusa/minplus/geometry.py
@@ -4,7 +4,22 @@ import math
 from collections.abc import Collection
 
 from .domain import validate_domain
-from .minplus import add, mult, add_matrices, MultivariatePolynomial
+from .minplus import add, mult, add_matrices
+
+
+def point_type(point : Collection, vertices : Collection, indexing_start : int = 1) -> Collection:
+    result = [[] for _ in range(len(vertices))]
+    point = np.array(point); vertices = np.array(vertices)
+    for i, vertex in enumerate(vertices):
+        comparison = add(*(vertex - point))
+        for j in range(len(vertex)):
+            if vertex[j] - point[j] == comparison:
+                result[j].append(indexing_start + i)
+    return result
+
+
+def hyperplane_from_apex(point : Collection[float|int]) -> Hyperplane:
+    return Hyperplane(*[-coordinate for coordinate in point])
 
 
 class Cone:
@@ -30,10 +45,6 @@ class Cone:
             result.append(self.get_point([float(s) for s in constants]))
         result = np.array(result)
         return result
-
-
-def hyperplane_from_apex(point : Collection[float|int]) -> Hyperplane:
-    return Hyperplane(*[-coordinate for coordinate in point])
 
 
 class Hyperplane:
@@ -66,22 +77,31 @@ class Polytope:
                     faces = [(0, *face) for face in faces]
                 self._validate_vertices_dimension(list(faces))
                 validate_domain(faces)
-            self.structure[rank] = faces
+            self.structure[rank] = np.array(faces)
 
     def _validate_vertices_dimension(self, vertices : list[Collection]) -> None:
         pivot = len(vertices[0])
         if pivot not in [self.dimension, self.dimension + 1]:
-            raise ValueError('Incorrect dimension of the vertices coordinates.')
+            raise ValueError('Incorrect dimension of the vertices\' coordinates.')
         for vertex in vertices:
             if len(vertex) != pivot:
-                raise ValueError('Incorrect dimension of the vertices coordinates.')
+                raise ValueError('Incorrect dimension of the vertices\' coordinates.')
 
     @property
-    def vertices(self) -> list[tuple]:
+    def vertices(self) -> np.ndarray:
         return self.structure[0]
 
     @property
-    def edges(self) -> Collection:
+    def pseudovertices(self) -> np.ndarray:
+        result = []
+        for line_segment in self.get_all_line_segments():
+            for point in line_segment:
+                if not np.any(np.all(self.vertices == point, axis=1)):  # if point not in vertices
+                    result.append(point)
+        return np.array(result)
+
+    @property
+    def edges(self) -> list:
         result = []
         for vertices_identifiers in self.structure[1]:
             result.append([])
@@ -89,10 +109,36 @@ class Polytope:
                 result[-1].append(self.vertices[identifier])
         return result
 
-    def get_hyperplanes(self) -> list:
-        """ Returns a list of the facet-defining hyperplanes. """
+    def get_line_segments(self, edge_index : int) -> np.ndarray:
+        """
+        Returns a list of points belonging to a line segment of the polytope.
+        It is not necessarily sorted.
+        """
+        result = []
+        x, y = self.edges[edge_index]
+        for i in range(len(x)):
+            if y[i] >= x[i]:
+                result.append(add_matrices((y[i] - x[i]) + x, y))
+            else:
+                result.append(add_matrices((x[i] - y[i]) + y, x))
+        return np.array(sorted(result, key=lambda x: x[-1] - x[-2]))
+
+    def get_all_line_segments(self) -> list:
+        result = []
+        for i in range(len(self.structure[1])):
+            result.append(self.get_line_segments(i))
+        return result
+
+    def get_envelope(self) -> list:
         pass
 
-    def get_apices(self) -> list:
-        """ Returns a list of apices corresponding to the half-spaces defined by the facet-defining hyperplanes. """
-        pass
+    def get_apices(self) -> np.ndarray:
+        """ Returns an array of apices corresponding to the half-spaces defined by the facet-defining hyperplanes. """
+        return self.pseudovertices
+
+    def get_hyperplanes(self) -> list:
+        """ Returns a list of the facet-defining hyperplanes. """
+        result = []
+        for apex in self.get_apices():
+            result.append(hyperplane_from_apex(apex))
+        return result
