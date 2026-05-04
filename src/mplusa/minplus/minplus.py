f08c03fd946d52bead502179e351b8680527bf16
Maksymilian Wiekiera <maksymilian3563@gmail.com>
2026-05-04 11:09:56 +0200

    Simplified exponentiation; added simple docstrings; improved kleene functions

--- src/mplusa/minplus/minplus.py
+++ src/mplusa/minplus/minplus.py
@@ -9,38 +9,58 @@ from .. import utils
 
 
 def add(*args : float) -> float:
+    """
+    Tropical addition; performs validation and calulcates the minimum value.
+    """
     validate_domain(args)
     return min(args)
 
 
 def mult(*args : float) -> float:
+    """
+    Tropical multiplication; performs validation and calculates the sum.
+    """
     validate_domain(args)
     return sum(args) if math.inf not in args else math.inf
 
 
-def power(a : float, k : int) -> float:
-    return mult(*[a for _ in range(k)])
+def power(*args : float) -> float:
+    """
+    Tropical exponentiation; performs validation and calculates the product.
+    """
+    validate_domain(args)
+    return math.prod(args) if math.inf not in args else math.inf
 
 
 def modulo(a : float, t : int) -> float:
+    """
+    Tropical modulo; performs validation and calculates the difference between
+    the arguemnt a and the largest multiply of t lesser than the minuend.
+    """
     validate_domain([a, t])
     if a < 0 or t < 0:
         raise ValueError('The modulo operator is only defined for positive numbers.')
-    if a == math.inf:
-        return math.inf
-    if a == 0:
-        return 0
-    if t == math.inf or t == 0:
+    if a in [math.inf, 0] or t in [math.inf, 0]:
         return a
     return a - (a // t) * t
 
 
-def add_matrices(A : np.ndarray, B : np.ndarray) -> np.ndarray:
-    validate_domain([A, B])
-    return np.minimum(A, B)
+def add_matrices(*args : np.ndarray) -> np.ndarray:
+    """
+    Tropical matrix addition; performs validation and calculates item-wise minimum of arrays.
+    """
+    validate_domain(args)
+    result = args[0]
+    for A in args[1:]:
+        result = np.minimum(result, A)
+    return result
 
 
 def mult_matrices(A : np.ndarray, B : np.ndarray) -> np.ndarray:
+    """
+    Tropical matrix multiplication; calculates equivalently to the linear matrix multiplication
+    but with tropical operations instead.
+    """
     if A.shape[1] != B.shape[0]:
         raise ValueError('Given matrices are not of MxN and NxP shapes.')
     result = np.zeros((A.shape[0], B.shape[1]))
@@ -51,16 +71,20 @@ def mult_matrices(A : np.ndarray, B : np.ndarray) -> np.ndarray:
 
 
 def power_matrix(A : np.ndarray, k : int) -> np.ndarray:
-    if k == 0:
-        result = unit_matrix(A.shape[0], A.shape[1])
-    else:
-        result = A.copy()
-        for _ in range(k):
-            result = mult_matrices(A, result)
+    """
+    Tropical matrix exponentiation; repeats tropical matrix multiplication k times.
+    """
+    result = unit_matrix(A.shape[0], A.shape[1])
+    for _ in range(k):
+        result = mult_matrices(A, result)
     return result
 
 
 def modulo_matrices(A : np.ndarray, b : np.ndarray) -> np.ndarray:
+    """
+    Tropical matrix modulo; calculates the modulo value between the rows of the matrix and
+    a vector.
+    """
     if b.shape[1] != 1:
         raise ValueError('Given matrix b is not a vertical vector of shape Mx1')
     if A.shape[0] != b.shape[0]:
@@ -91,42 +115,69 @@ def mult_arrays(A : np.ndarray, B : np.ndarray) -> np.ndarray:
 
 
 def unit_matrix(width : int, height : int) -> np.ndarray:
+    """
+    Generates a tropical unit matrix of a given width and height.
+    """
     result = np.eye(width, height)
     result[result == 0] = math.inf
     result[result == 1] = 0
     return result
 
 
-def kleene_star(A : np.ndarray, iterations : int|None = None) -> np.ndarray:
-    if A.shape[0] != A.shape[1]:
-        raise ValueError('Matrix is not square.')
-    series = [
-        unit_matrix(A.shape[0], A.shape[1]),
-        A.copy()
-    ]
-    result = add_matrices(series[0], series[1])
-    if iterations is None:
-        iterations = int(result.shape[0])
-    for i in range(iterations):
-        series.append(power_matrix(A, i))
-        result = add_matrices(result, series[-1])
+def kleene_star(A : np.ndarray, iterations : int = -1) -> np.ndarray:
+    """
+    Calculates the value of tropical Kleene star for a given matrix.
+    """
+    result = unit_matrix(*A.shape)
+    if iterations < 0:
+        iterations = A.shape[0] + 1
+    for i in range(1, iterations):
+        last = power_matrix(A, i)
+        result = add_matrices(result, last)
     return result
 
 
-def kleene_plus(A : np.ndarray, iterations : int|None = None) -> np.ndarray:
-    if A.shape[0] != A.shape[1]:
-        raise ValueError('Matrix is not square.')
-    series = [A.copy()]
-    result = series[0]
-    if iterations is None:
-        iterations = int(result.shape[0])
+def kleene_star_series(A : np.ndarray, iterations : int = -1) -> list:
+    """
+    Calculates all matrices generated during the calculation of tropical Kleene star.
+    """
+    series = [unit_matrix(*A.shape)]
+    if iterations < 0:
+        iterations = A.shape[0] + 1
     for i in range(1, iterations):
         series.append(power_matrix(A, i))
-        result = add_matrices(result, series[-1])
+    return series
+
+
+def kleene_plus(A : np.ndarray, iterations : int = -1) -> np.ndarray:
+    """
+    Calculates the value of tropical Kleene plus for a given matrix.
+    """
+    result = A.copy()
+    if iterations < 0:
+        iterations = A.shape[0] + 1
+    for i in range(2, iterations):
+        last = power_matrix(A, i)
+        result = add_matrices(result, last)
     return result
+
+
+def kleene_plus_series(A : np.ndarray, iterations : int = -1) -> list:
+    """
+    Calculates all matrices generated during the calculation of tropical Kleene plus.
+    """
+    series = []
+    if iterations < 0:
+        iterations = A.shape[0] + 1
+    for i in range(1, iterations):
+        series.append(power_matrix(A, i))
+    return series
 
 
 def tdet(A : np.ndarray) -> float:
+    """
+    Calculates the tropical determinant of a matrix.
+    """
     used = np.zeros(A.shape[0])
     best = math.inf
 
@@ -155,6 +206,9 @@ def tdet(A : np.ndarray) -> float:
 
 
 def power_algorithm(A : np.ndarray, x_0 : np.ndarray|None = None, iterations : int = 1000) -> tuple:
+    """
+    An implementation of the power algorithm for finding the eigenvalue of a matrix.
+    """
     if x_0 is None:
         x_0 = np.ones((A.shape[1], 1))
     xs = [x_0]
@@ -170,7 +224,9 @@ def power_algorithm(A : np.ndarray, x_0 : np.ndarray|None = None, iterations : i
 
 
 def karp_algorithm(A : np.ndarray) -> float:
-    """ An implementation of Karp's algorithm for finding the minimum cycle mean. """
+    """
+    An implementation of Karp's algorithm for finding the minimum cycle mean.
+    """
     n = A.shape[0]
     dp = np.zeros((n + 1, n))
     for i in range(1, n + 1):
@@ -190,7 +246,9 @@ def karp_algorithm(A : np.ndarray) -> float:
 
 
 def kleene_star_algorithm(A : np.ndarray, column : int = 0) -> np.ndarray:
-    """ Returns a non-normalized eigenvector of a tropical matrix. """
+    """
+    Returns a non-normalized eigenvector of a tropical matrix.
+    """
     l = karp_algorithm(A)  # the minimum tropical cycle mean -- assumed eigenvector
     B = np.add(A, -l)
     B_star = kleene_star(B)
@@ -198,7 +256,9 @@ def kleene_star_algorithm(A : np.ndarray, column : int = 0) -> np.ndarray:
 
 
 def modified_kleene_star_algorithm(A : np.ndarray) -> np.ndarray:
-    """ A modified version of the Kleene star algorithm which uses Kleene plus to locate the critical column. """
+    """
+    A modified version of the Kleene star algorithm which uses Kleene plus to locate the critical column.
+    """
     l = karp_algorithm(A)  # the minimum tropical cycle mean -- assumed eigenvector
     B = np.add(A, -l)
     B_star = kleene_star(B)
@@ -207,7 +267,9 @@ def modified_kleene_star_algorithm(A : np.ndarray) -> np.ndarray:
 
 
 def eigenvalue(A : np.ndarray, func : Callable = power_algorithm) -> float:
-    """ Calculate the eigenvalue of a matrix using one of the provided methods. """
+    """
+    Calculate the eigenvalue of a matrix using one of the provided methods.
+    """
     if func.__name__ == 'power_algorithm':
         p, q, c, _ = func(A)
         return c / (p - q)
@@ -217,7 +279,9 @@ def eigenvalue(A : np.ndarray, func : Callable = power_algorithm) -> float:
 
 
 def normalize_vector(v : np.ndarray) -> np.ndarray:
-    """ Normalize a vector by substracting the minimal value. """
+    """
+    Normalize a vector by substracting the minimal value.
+    """
     validate_domain(v)
     if len(v.shape) == 1:
         return v - add(*v)
@@ -228,7 +292,9 @@ def normalize_vector(v : np.ndarray) -> np.ndarray:
 
 
 def eigenvector(A : np.ndarray, func : Callable = power_algorithm, func_kwargs : dict = {}, normalize : bool = True) -> np.ndarray:
-    """ Calculate the eigenvector of a matrix using one of the provided methods. """
+    """
+    Calculate the eigenvector of a matrix using one of the provided methods.
+    """
     if func.__name__ == 'power_algorithm':
         p, q, c, xs = power_algorithm(A, **func_kwargs)
         eigenvalue = c / (p - q)
@@ -245,7 +311,9 @@ def eigenvector(A : np.ndarray, func : Callable = power_algorithm, func_kwargs :
 
 
 class MultivariatePolynomial:
-    """ An implementation of a tropical polynomial with multiple variables. """
+    """
+    An implementation of a tropical polynomial with multiple variables.
+    """
 
     def __init__(self, coefficients : np.ndarray) -> None:
         validate_domain(coefficients)
@@ -283,7 +351,9 @@ class MultivariatePolynomial:
         return result[:-3]
 
     def get_linear_hyperplanes(self) -> list[list[float|int]]:
-        """ Returns a list of coefficients of a linear equation for every hyperplane building the polynomial. """
+        """
+        Returns a list of coefficients of a linear equation for every hyperplane building the polynomial.
+        """
         result = []
         for indices, coefficient in np.ndenumerate(self.coefficients):
             if coefficient == math.inf:
@@ -303,14 +373,18 @@ class MultivariatePolynomial:
 
 
 class Polynomial(MultivariatePolynomial):
-    """ An implementation of a tropical polynomial with a single variable. """
+    """
+    An implementation of a tropical polynomial with a single variable.
+    """
 
     def __init__(self, *coefficients : float|int) -> None:
         validate_domain(coefficients)
         super().__init__(np.array(coefficients))
 
     def get_line_intersections(self) -> list[list[float|int]]:
-        """ Returns a list of intersection points for the lines building the polynomial. """
+        """
+        Returns a list of intersection points for the lines building the polynomial.
+        """
         result = []
         lines = self.get_linear_hyperplanes()  # Hyperplanes are lines in this case
         for line in lines:  # Change the form of the equation to a + bx from a + bx + cy
@@ -324,7 +398,9 @@ class Polynomial(MultivariatePolynomial):
         return result
 
     def get_roots(self) -> tuple[list[float|int], list[int]]:
-        """ Returns lists of roots of the polynomial and of their respective ranks (the amount of monomials attaining the value). """
+        """
+        Returns lists of roots of the polynomial and of their respective ranks (the amount of monomials attaining the value).
+        """
         result = {}
         points = self.get_line_intersections()
         for point in points:
